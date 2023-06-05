c96eaa04ef8ce5e694cf4ab187b6ad91fdc2980d
Liquidzk <492928615@qq.com>
2023-06-05 20:09:51 +0800

    form

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
-
+import './assets/gloabal.css'
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, {size: "small"});
 
 new Vue({
   router,
